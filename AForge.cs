0a03230a1648c5a69c69d9175f69094c806d1b07
Krishanu Maity <krishanumaity61@gmail.com>
2021-08-26 11:21:30 +0530

    Adding Barcode Generation Code

---
+++ AForge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Zen.Barcode;
+
+namespace Utils
+{
+    public class AForge : Imagery
+    {
+        //Implementation of Barcode Generation
+        public void GenerateBarcode(string BarText)
+        {
+            Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            var image = barcode.Draw(BarText, 50);
+            Bitmap bitmap = new Bitmap(image);
+            using (var graphics = Graphics.FromImage(image))
+            using (var font = new Font("Consolas", 12)) // Any font you want
+            using (var brush = new SolidBrush(Color.White))
+            using (var format = new StringFormat() { LineAlignment = StringAlignment.Far }) // To align text above the specified point
+            {
+                
+                graphics.DrawString(BarText, font, brush, 0, image.Height, format);
+            }
+            bitmap.Save("barcode.tif", System.Drawing.Imaging.ImageFormat.Tiff);
+        }
+    }
+}
---
+++ Imagery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public interface Imagery
+    {
+        void GenerateBarcode(string BarText); //Barcode Generation of a given text
+    }
+}
